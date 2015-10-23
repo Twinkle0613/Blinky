763f732230848495efaf48b197ddfa35025b0138
Twinkle <twinkle0613@live.com>
2015-10-23 15:38:24 +0800

    New Update

--- app/App/main.c
+++ app/App/main.c
@@ -13,14 +13,21 @@ void delay(uint32_t delayCount){
 
 int main(){
 
+	configurePin(GPIO_OUTPUT,GPIO_PIN_14,GPIOG);
 	configurePin(GPIO_OUTPUT,GPIO_PIN_13,GPIOG);
-
+	configurePin(GPIO_OUTPUT,GPIO_PIN_5,GPIOC);
+	configurePin(GPIO_OUTPUT,GPIO_PIN_13,GPIOB);
 	while(1){
-	delay(1000000);
-	writeOne(GPIO_PIN_13,GPIOG);
-	delay(1000000);
+	delay(100000);
+	writeOne(GPIO_PIN_14,GPIOG);
 	writeZero(GPIO_PIN_13,GPIOG);
-
+	writeZero(GPIO_PIN_5,GPIOC);
+	writeZero(GPIO_PIN_13,GPIOB);
+	delay(100000);
+	writeZero(GPIO_PIN_14,GPIOG);
+	writeOne(GPIO_PIN_13,GPIOG);
+	writeOne(GPIO_PIN_5,GPIOC);
+	writeOne(GPIO_PIN_13,GPIOB);
 	}
 
 	}
--- app/Gpio.c
+++ app/Gpio.c
@@ -3,31 +3,42 @@
 
 
 configurePin(int direction, int pinNum, GPIO_TypeDef *port){
-	 __HAL_RCC_GPIOG_CLK_ENABLE();
 	GPIO_InitTypeDef gpio;
+	 __HAL_RCC_GPIOG_CLK_ENABLE();
+	 __HAL_RCC_GPIOC_CLK_ENABLE();
+	 __HAL_RCC_GPIOB_CLK_ENABLE();
+	 int *ptr = (int*)0x40021800;
+	 ptr[0] =0X14000000;  //output = 14 and 13
+	 ptr[1] =0x00000000;
+	 ptr[2] =0X28000000;
+	 int *ptr2 = (int*)0x40020800; //PC
+	 ptr2[0] =0X00000400;  //output = 5
+	 ptr2[1] =0x00000000;
+	 ptr2[2] =0X00000800;
+	 int *ptr3 = (int*)0x40020400; //PB
+	 ptr3[0] =0X04000000;  //output = 13
+	 ptr3[1] =0x00000000;
+	 ptr3[2] =0X08000000;
+
+
+	/*
 	gpio.Pin = pinNum;
 	gpio.Mode =direction;
 	gpio.Pull = GPIO_NOPULL;
 	gpio.Speed = GPIO_SPEED_MEDIUM;
-
 	HAL_GPIO_Init(port,&gpio);
-
+    */
 }
 
 
 void writeOne(int pinNum, GPIO_TypeDef *port){
-
-
 	HAL_GPIO_WritePin(port,pinNum, GPIO_PIN_SET);
-
-
 }
 
 
 void writeZero(int pinNum, GPIO_TypeDef *port){
-
-
 	HAL_GPIO_WritePin(port,pinNum, GPIO_PIN_RESET);
-
-
 }
+
+
+
